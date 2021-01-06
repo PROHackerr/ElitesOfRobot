57a1b110fea5cca948f5dd79ffde80b80c867c15
Divu <70699947+imdivu@users.noreply.github.com>
2021-01-06 12:37:52 +0530

    Create __init__.py

---
+++ ElitesOfRobot/modules/helper_funcs/telethon/__init__.py
@@ -0,0 +1,6 @@
+from ElitesOfRobot import (DEV_USERS, SUDO_USERS, SUPPORT_USERS, 
+                           WHITELIST_USERS, telethon)
+
+STAFF_USERS = SUDO_USERS + SUPPORT_USERS + WHITELIST_USERS + DEV_USERS
+
+STAFF_USERS = list(SUDO_USERS) + list(SUPPORT_USERS) + list(WHITELIST_USERS) + list(DEV_USERS)                            
